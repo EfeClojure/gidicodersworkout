36d8c356dfc86f961a42c074c34ead5a837fcacf
Efe Ariaroo <efeariaroo@gmail.com>
2015-09-11 00:43:49 +0100

    I think I got the database relationship right at this point ... will continue tomorrow

--- src/gidicodersworkout/db/dbaccess.clj
+++ src/gidicodersworkout/db/dbaccess.clj
@@ -1,4 +1,4 @@
-(ns dbaccess
+(ns gidicodersworkoutdb.db.dbaccess
   (:require [immutant.web :as immutant]
             [korma.core :as korma-core])
   (:use [korma.db :only (defdb postgres)]))
@@ -9,12 +9,14 @@
                          :user "postgres"
                          :password "asdffdsa"}))
 #_(defdb the-db (postgres {:db (or (System/getenv "DATABASE_URL")
-                                 "postgresql://localhost:5432/gidicodersworkout")
-                         :user "postgres"
-                         :password "asdffdsa"}))
+                                   "postgresql://localhost:5432/gidicodersworkout")
+                           :user "postgres"
+                           :password "asdffdsa"}))
 
 (declare workouts users languages roles 
-         workout-entries workout-persons user-roles)
+         workout-entries)
+
+;; valid time stamp: '2004-10-19 10:23:54'
 
 (korma-core/defentity workouts 
   (korma-core/pk :workout_id)
@@ -22,7 +24,7 @@
   (korma-core/table :workout)
   (korma-core/entity-fields :workout_title :workout_text 
                             :start_date :end_date :is_active)
-  (korma-core/has-many users))
+  (korma-core/has-many workout-entries))
 
 (korma-core/defentity users
   (korma-core/pk :user_id) 
@@ -31,13 +33,13 @@
   (korma-core/entity-fields :first_name :last_name :email_address 
                             :username :password)
 
-  (korma-core/has-many roles))
+  (korma-core/has-one roles {:fk :role_id}))
 
 (korma-core/defentity roles 
   (korma-core/pk :role_id)
   (korma-core/table :role)
-  (korma-core/entity-fields :role_title)
 
+  (korma-core/entity-fields :role_title)
   (korma-core/belongs-to users))
 
 (korma-core/defentity languages
@@ -50,11 +52,11 @@
   (korma-core/pk :workout_entry_id)
 
   (korma-core/table :workout_entry)
-  (korma-core/entity-fields :source_text)
+  (korma-core/entity-fields :source_text :date_sent)
   
-  (korma-core/belongs-to workouts)
-  (korma-core/has-one users)
-  (korma-core/has-one languages))
+  (korma-core/belongs-to workouts {:fk :workout_id})
+  (korma-core/has-one users {:fk :user_id})
+  (korma-core/has-one languages {:fk :language_id}))
 
 
 ;; -- 
@@ -63,7 +65,9 @@
   #_(with address)
   (korma-core/fields :role_title))
 
-
+(korma-core/dselect languages
+  #_(with address)
+  (korma-core/fields :language_name))
 
 
 
