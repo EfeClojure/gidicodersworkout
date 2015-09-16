67af77392ed3d4358dbc24f76910b41c6c7a0ab4
Efe Ariaroo <efeariaroo@gmail.com>
2015-09-16 18:24:19 +0100

    Improvements to the dialog for creating a new workout ... removed the users migrations sql files.

--- src/gidicodersworkout/routes/home.clj
+++ src/gidicodersworkout/routes/home.clj
@@ -2,11 +2,21 @@
   (:require [gidicodersworkout.layout :as layout]
             [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
+            [ring.util.response :refer [response]]
             [clojure.java.io :as io]
             [gidicodersworkout.db.dbaccess :as the-db]
             
             [digest :as digest]))
 
+
+(defonce username-exists "That username already exists.")
+(defonce password-no-match "Password does NOT match.")
+
+(defn render-with-cookie [page-response cookie-key cookie-val]
+  (println "cookie-key : " cookie-key " cookie-val: " cookie-val)
+  (-> page-response 
+      response 
+      (assoc :cookie {cookie-key {:value cookie-val}})))
 
 
 (defn login-page []
@@ -14,35 +24,33 @@
 
 
 (defn home-page [user-record]
-  #_(layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)})
-  
-  (layout/render "homePage.html"))
+  (let [page-response (layout/render "homePage.html" 
+                                {:user user-record})]
+    (render-with-cookie page-response
+                        "username" (user-record :username))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 
 (defn index-page [the-request]
-  (let [the-coders (the-db/get-users)
-        the-cookies (:cookies the-request)
+  (let [the-cookies (:cookies the-request)
+        the-coders (the-db/get-users)
         the-workouts (the-db/get-workouts)]
     (if (empty? the-cookies)
       (layout/render "index.html" {:coders the-coders 
                                    :workouts the-workouts})
-      (let [userid-cookie (the-cookies :userId)]
-        (if userid-cookie
-          (let [the-user (the-db/get-user-by-id userid-cookie)]
+      (let [username-cookie (the-cookies "username")]
+        (if username-cookie
+          (let [the-user (the-db/get-user-by-name username-cookie)]
             (if (empty? the-user)
-              (do (println "Couldn't find user with userId: " userid-cookie)
+              (do (println "Couldn't find user with username: " username-cookie)
                   (layout/render "index.html" {:coders the-coders
                                                :workouts the-workouts}))
               (do 
-                (println "Got the user with userId: " userid-cookie) 
-                (layout/render "index.html" (:user the-user
-                                                   :coders the-coders
-                                                   :workouts the-workouts)))))
-          (do (println "No cookies with :userId key")
+                (println "Got the user with username: " username-cookie) 
+                (home-page the-user))))
+          (do (println "No cookies with :username key")
             (layout/render "index.html" 
                            {:coders the-coders
                             :workouts the-workouts})))))))
@@ -51,21 +59,20 @@
 (defn logUserIn [req username password]
   (let [the-user (the-db/get-user-by-name username)]
     (if the-user
-
       (if (= (digest/md5 password) 
              (:password the-user))
         (home-page the-user)
         (layout/render "loginScreen.html"
-                       {:auth_error "Password doesn't match"}))
+                       {:auth_error password-no-match}))
       (layout/render "loginScreen.html" 
-                     {:auth_error "Username does NOT exist!"}))))
+                     {:auth_error username-exists}))))
 
 (defn signUserUp [first-name last-name username 
                   email-add password]
   (let [the-user (the-db/get-user-by-name username)]
     (if the-user
       (layout/render "loginScreen.html" 
-                     {:auth_error "A user with that username already exists!"})
+                     {:auth_error username-exists})
       (do 
         (let [new-user (the-db/add-user first-name last-name 
                                         username email-add password)]
@@ -79,9 +86,6 @@
   (layout/render "createWorkout.html"))
 
 
-#_(defn set-cookie []
-  (-> "cookie set" response (update-in [:cookies "username" :value] "Alice"))
-  {:status 200 :body "Hello World"})
 
 
 
