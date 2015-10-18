826899f285a788ff0855417234aba127244058bd
Ariaroo Efe <efeariaroo@gmail.com>
2015-10-18 16:11:31 +0100

    Made errors display on home view when trying to make a submission to a workout

--- src/gidicodersworkout/routes/home.clj
+++ src/gidicodersworkout/routes/home.clj
@@ -11,7 +11,8 @@
                                                warn error fatal]]
             [selmer.filters :as filters]
             [digest :as digest]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json])
+  (:import [java.net URLEncoder]))
 
 
 (defonce username-no-exists "That username does NOT exist.")
@@ -32,7 +33,7 @@
   (layout/render "loginScreen.html"))
 
 
-(defn home-page [user-id]
+(defn home-page [user-id _error]
   (let [the-user (the-db/get-user-by-id user-id)
         my-workouts (the-db/get-workout-by-userId user-id)
         my-entries (the-db/get-user-entries-by-userId user-id)
@@ -41,7 +42,8 @@
                    {:user the-user
                     :all-workouts all-workouts
                     :my-workouts my-workouts
-                    :my-entries my-entries})))
+                    :my-entries my-entries
+                    :_error _error})))
 
 #_(defn get-workout-details [workout-id] 
   (let [the-workout (the-db/get-workout-by-id workout-id)]
@@ -76,14 +78,15 @@ Will return nil if the language is non-existent"
         submitted-before (the-db/user-submitted? workoutId userId)]
     (if (not submitted-before)
       (do 
-        (the-db/add-workout-entry workoutId userId 
-                                  lang-id sourceText)
-        (found (str "/homePage?userId=" userId)))
+        (let [added-entry (the-db/add-workout-entry workoutId userId 
+                                                    lang-id sourceText)]
+          (timbre/info "Added entry: " added-entry))
+        (found (str "/homePage?userId=" userId "&_error=")))
       (do (timbre/info "entry already made for workout" workoutId
                        " by user: " userId) 
-          (found (str "/homePage?userId=" userId))
-          #_(home-page (the-db/get-user-by-id userId)  
-                       {:_error "User has already made a submission!"})))))
+          (found (str "/homePage?userId=" userId "&_error=" 
+                      (URLEncoder/encode "The user has already made a submission" 
+                                         "UTF-8")))))))
 
 (defn index-page [the-request]
   (let [the-coders (the-db/get-users)
@@ -99,7 +102,7 @@ Will return nil if the language is non-existent"
              (:password the-user))
         #_(home-page (the-user :user_id))
         (found (str "/homePage?userId=" 
-                    (the-user :user_id)))
+                    (the-user :user_id) "&_error="))
         (layout/render "loginScreen.html"
                        {:auth_error password-no-match}))
       (layout/render "loginScreen.html" 
@@ -114,8 +117,8 @@ Will return nil if the language is non-existent"
       (do 
         (let [new-user (the-db/add-user first-name last-name 
                                         username email-add password)]
-          (found (str "/homePage?userId=" (the-user :user_id)))
-          #_(home-page new-user))))))
+          (found (str "/homePage?userId=" (the-user :user_id) 
+                      "&_error=")))))))
 
 
 (defn create-workout [username title desc start end]
@@ -125,8 +128,11 @@ Will return nil if the language is non-existent"
     (if the-user
       (do 
         (the-db/add-workout (the-user :user_id) title desc start end)
-        (found (str "/homePage?userId=" (the-user :user_id))))
-      (found (str "/homePage?userId=" (the-user :user_id))))))
+        (found (str "/homePage?userId=" (the-user :user_id) 
+                    "&_error=")))
+      (found (str "/homePage?userId=" (the-user :user_id) 
+                  "&_error=" (URLEncoder/encode "Who are you? You don't exist!" 
+                                         "UTF-8"))))))
 
 (defn view-all-entries [workout-id workout-title]
   (let [the-entries (the-db/get-workout-entries workout-id)]
@@ -144,8 +150,8 @@ Will return nil if the language is non-existent"
   (GET "/about" [] (about-page))
 
   (GET "/login" [] (login-page))
-  (GET "/homePage" [userId] 
-       (home-page userId))
+  (GET "/homePage" [userId _error] 
+       (home-page userId _error))
 
   (POST "/logUserIn" [username password] 
         (fn [req] (logUserIn req username password)))
